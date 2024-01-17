3bc6c78dcaf918ee8df1a07845fcf005da3e8e81
Laurel Schwulst <laurel.schwulst@gmail.com>
2024-01-17 18:05:55 -0500

    cms connection

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy('assets');
+
+    eleventyConfig.setTemplateFormats(["md", "njk"]);
+
 };
