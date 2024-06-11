5178af01bb63ae898893dc5d90438ac7b00b2788
Taichi Aritomo <taichiaritomo@gmail.com>
2024-06-11 10:35:58 -0400

    Revert "Push docs folder build"
    This reverts commit 39fe3620407807ebc2bc8022a62dc9d3cd24df98.

--- .eleventy.js
+++ .eleventy.js
@@ -10,11 +10,4 @@ module.exports = function (eleventyConfig) {
       return collection.getAll().filter(item => item.inputPath.startsWith('./selfies/')).sort((a, b) => Math.random() - 0.5);
     });
 
-    eleventyConfig.ignores.add('/_docs/**');
-    eleventyConfig.ignores.add('README.md');
-    return {
-      dir: {
-        output: "docs" // This changes the output directory from _site to docs (for Github pages to serve the site from the docs folder)
-      }
-    }
 };
