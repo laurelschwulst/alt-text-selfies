b6f8345939096e081a90e33de330c5111d6328fa
Taichi Aritomo <taichiaritomo@gmail.com>
2024-06-11 10:40:16 -0400

    Build for staging

--- _data/site.js
+++ _data/site.js
@@ -3,6 +3,10 @@ module.exports = function() {
     return {
       url: "http://localhost:8080",
     }
+  } else if (process.env.NODE_ENV === "staging") {
+    return {
+      url: "https://staging.alttextselfies.net"
+    }
   } else {
     return {
       url: "https://alttextselfies.net",
