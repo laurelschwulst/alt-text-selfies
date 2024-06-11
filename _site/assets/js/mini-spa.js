08f60c31acdb121abd2ea1dd6a6c3725e6e197b3
Taichi Aritomo <taichiaritomo@gmail.com>
2024-06-11 10:35:34 -0400

    Revert "Build to prod with _site"
    This reverts commit 29857954318f44cbe937192aca21424fdd033092.

--- _site/assets/js/mini-spa.js
+++ _site/assets/js/mini-spa.js
@@ -57,10 +57,8 @@
     links.forEach((link) => {
       if (link.href === newHref) {
         link.classList.add('active');
-        link.setAttribute('aria-current', 'page');
       } else {
         link.classList.remove('active');
-        link.removeAttribute('aria-current');
       }
     });
   }
